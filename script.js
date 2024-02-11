9fa424d62b61778ba93b892f618f8ee1a2b25267
Stockriderj <jason.qian.36@gmail.com>
2024-02-11 17:09:41 -0600

    begin race dev

--- script.js
+++ script.js
@@ -3,11 +3,20 @@ const ctx = canvas.getContext("2d");
 
 const CANVAS_WIDTH = (canvas.width = 600);
 const CANVAS_HEIGHT = (canvas.height = 600);
+const BACKGROUND_WIDTH = 5620;
+const BACKGROUND_HEIGHT = 4900;
+const backgroundImage = new Image();
+backgroundImage.src =
+  "https://media.istockphoto.com/id/1333794966/vector/top-down-racing-circuit-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=fEvS411Mvc2TSmAc0zULCX1_yvZZXTV_1Zsz99_ND0A=";
 
-const MOVE_SPEED = 2;
+const MOVE_SPEED = 0.2;
+const MAX_SPEED = 10;
+const PLAYER_SIZE = 50;
 
-let x = 275;
-let y = 275;
+const PLAYER_CANVAS = CANVAS_WIDTH / 2 - PLAYER_SIZE / 2;
+
+let x = 200;
+let y = 200;
 
 let speedX = 0;
 let speedY = 0;
@@ -62,24 +71,18 @@ window.addEventListener(
 );
 
 function handleKeyPress() {
-  if (pressedW) {
-    if (touchingBorder.bottom) {
-      // if touching ground
-      speedY -= MOVE_SPEED * 10;
-    }
-  } //jump
-  if (pressedA) speedX -= MOVE_SPEED;
-  if (pressedS) speedY += MOVE_SPEED;
-  if (pressedD) speedX += MOVE_SPEED;
+  if (pressedW) speedY = Math.max(-MAX_SPEED, speedY - MOVE_SPEED);
+  if (pressedA) speedX = Math.max(-MAX_SPEED, speedX - MOVE_SPEED);
+  if (pressedS) speedY = Math.min(MAX_SPEED, speedY + MOVE_SPEED);
+  if (pressedD) speedX = Math.min(MAX_SPEED, speedX + MOVE_SPEED);
 }
 
 function handleCollisions() {
-  const BORDER_WIDTH = 50;
   touchingBorder = {
     top: y <= 0,
-    bottom: y >= CANVAS_HEIGHT - BORDER_WIDTH,
+    bottom: y >= BACKGROUND_HEIGHT - PLAYER_SIZE,
     left: x <= 0,
-    right: x >= CANVAS_WIDTH - BORDER_WIDTH,
+    right: x >= BACKGROUND_WIDTH - PLAYER_SIZE,
   };
 
   if (touchingBorder.left) {
@@ -87,16 +90,16 @@ function handleCollisions() {
     speedX = 0;
   }
   if (touchingBorder.right) {
-    x = CANVAS_WIDTH - BORDER_WIDTH;
+    x = BACKGROUND_WIDTH - PLAYER_SIZE;
     speedX = 0;
   }
-//   if (touchingBorder.top) {
-//     y = 0;
-//     if (speedY < 0) speedY = 0;
-//   }
+  if (touchingBorder.top) {
+    y = 0;
+    speedY = 0;
+  }
   if (touchingBorder.bottom) {
-    y = CANVAS_HEIGHT - BORDER_WIDTH;
-    if (speedY > 0) speedY = 0;
+    y = BACKGROUND_HEIGHT - PLAYER_SIZE;
+    speedY = 0;
   }
 }
 
@@ -109,18 +112,29 @@ function main() {
 
   console.log(speedX, speedY, x, y);
 
-  speedY += 1; // gravity
-  if (speedX > 0) speedX--;
-  else if (speedX < 0) speedX++;
+  //   speedY += 1; // gravity
+  if (speedX > 0) speedX -= 0.1;
+  else if (speedX < 0) speedX += 0.1;
+  if (speedY > 0) speedY -= 0.1;
+  else if (speedY < 0) speedY += 0.1;
 
-  speedX = Math.floor(speedX);
-  speedY = Math.floor(speedY);
+  speedX = Math.round(speedX * 1000) / 1000;
+  speedY = Math.round(speedY * 1000) / 1000;
 
   x += speedX;
   y += speedY;
 
+  ctx.drawImage(
+    backgroundImage,
+    -x + PLAYER_CANVAS,
+    -y + PLAYER_CANVAS,
+    BACKGROUND_WIDTH,
+    BACKGROUND_HEIGHT
+  );
+
   // Push the current position to the lastPositions array
-  lastPositions.push({x: x, y: y});
+  lastPositions.push({x: PLAYER_CANVAS, y: PLAYER_CANVAS});
+  //   lastPositions.push({x: x, y: y});
 
   // If there are more than 3 positions in the array, remove the oldest one
   if (lastPositions.length > 20) {
@@ -131,7 +145,7 @@ function main() {
   lastPositions.forEach((pos, index) => {
     let colorValue = Math.floor(255 - (index / 20) * 255);
     ctx.fillStyle = `rgb(${colorValue}, ${colorValue}, ${colorValue})`;
-    ctx.fillRect(pos.x, pos.y, 50, 50);
+    ctx.fillRect(pos.x, pos.y, PLAYER_SIZE, PLAYER_SIZE);
   });
   ctx.fillStyle = "black";
 
