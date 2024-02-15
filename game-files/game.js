41b10dad3ae62d7bf9fdb0e1d65c9b3ea4c65c5a
Stockriderj <jason.qian.36@gmail.com>
2024-02-14 21:31:34 -0600

    significant code reworks + zoom out during accel

--- game-files/game.js
+++ game-files/game.js
@@ -16,34 +16,55 @@ function e() {
 
   // constants
   const zoomRange = document.getElementById("zoomRange");
-  let ZOOM = zoomRange.value / 10;
-  // zoomRange.addEventListener("change", () => {
-  //   ZOOM = zoomRange.value / 10;
-  //   console.log(ZOOM);
-  // });
+  let BASE_ZOOM = zoomRange.value / 10;
+  let ZOOM = BASE_ZOOM;
+  zoomRange.addEventListener("change", () => {
+    BASE_ZOOM = zoomRange.value / 10;
+  });
+
+  // const BASE_ZOOM = {
+  //   value: zoomRange.value / 10,
+
+  //   scale: function (val) {
+  //     return val * this.value;
+  //   },
+  //   unscale: function (val) {
+  //     return val / this.level;
+  //   },
+  //   rescale: function (original, val) {
+  //     return (val / original) * this.level;
+  //   },
+  //   update: function () {
+  //     this.level = zoomRange.value / 10;
+  //   },
+  // };
 
   const CANVAS_WIDTH = (canvas.width = 600);
   const CANVAS_HEIGHT = (canvas.height = 600);
-  const BACKGROUND_WIDTH = 16830 * ZOOM;
-  const BACKGROUND_HEIGHT = 14700 * ZOOM;
+
+  const CENTER_X = CANVAS_WIDTH / 2;
+  const CENTER_Y = CANVAS_HEIGHT / 2;
+
+  const BACKGROUND_WIDTH = 16830;
+  const BACKGROUND_HEIGHT = 14700;
   const backgroundImage = new Image();
   backgroundImage.src =
     "https://media.istockphoto.com/id/1333794966/vector/top-down-racing-circuit-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=fEvS411Mvc2TSmAc0zULCX1_yvZZXTV_1Zsz99_ND0A=";
 
-  const ACCEL_SPEED = 0.1 * ZOOM;
-  const MAX_SPEED = 12 * ZOOM;
+  const ACCEL_SPEED = 0.1;
+  const MAX_SPEED = 12;
   const BASE_STEER_SPEED = 4;
-  const MAX_STEER = 48;
-  const PLAYER_SIZE = 40 * ZOOM;
+  const MAX_STEER = 40;
+  const PLAYER_SIZE = 40;
 
-  const PLAYER_CANVAS = CANVAS_WIDTH / 2 - PLAYER_SIZE / 2;
+  const PLAYER_CANVAS = CENTER_X - PLAYER_SIZE / 2;
 
-  let x = 5850 * ZOOM;
-  let y = 5000 * ZOOM;
+  let x = 5850;
+  let y = 5000;
 
   let speed = 0;
   let mphSpeed = 0; // MAX REAL SPEED = 220 mph / MAX GAME SPEED = 2 = 110 * speed
-  const speedFriction = 0.01 * ZOOM;
+  const speedFriction = 0.01;
   let steerSpeed;
   let steering = 180;
   let steerDirection = 0;
@@ -120,30 +141,30 @@ function e() {
   function drawArrow() {
     ctx.beginPath();
     ctx.strokeStyle = "blue";
-    ctx.moveTo(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2);
+    ctx.moveTo(CENTER_X, CENTER_Y);
     ctx.lineTo(
-      CANVAS_WIDTH / 2 + 50 * Math.cos((steering * Math.PI) / 180),
-      CANVAS_HEIGHT / 2 + 50 * Math.sin((steering * Math.PI) / 180)
+      CENTER_X + 50 * Math.cos((steering * Math.PI) / 180),
+      CENTER_Y + 50 * Math.sin((steering * Math.PI) / 180)
     );
     ctx.stroke();
 
     // Start a new path for the black line
     ctx.beginPath();
     ctx.strokeStyle = "black";
-    ctx.moveTo(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2);
+    ctx.moveTo(CENTER_X, CENTER_Y);
 
     // Make the black line follow the direction of the blue line
     let blackSteering = steering + steerDirection;
     ctx.lineTo(
-      CANVAS_WIDTH / 2 + 20 * Math.cos((blackSteering * Math.PI) / 180),
-      CANVAS_HEIGHT / 2 + 20 * Math.sin((blackSteering * Math.PI) / 180)
+      CENTER_X + 20 * Math.cos((blackSteering * Math.PI) / 180),
+      CENTER_Y + 20 * Math.sin((blackSteering * Math.PI) / 180)
     );
     ctx.stroke();
   }
 
   function drawStats() {
     ctx.font = "bold 35pt Tahoma";
-    ctx.fillText(`Speed: ${Math.round((speed * 30) / ZOOM)}mph`, 10, 40);
+    ctx.fillText(`Speed: ${Math.round(speed * 30)}mph`, 10, 40);
     ctx.font = "bold 10pt Tahoma";
     ctx.fillText(`raw speed: ${speed}px/ps`, 10, 100);
     ctx.fillText(`direction ${steering}deg`, 10, 120);
@@ -197,7 +218,7 @@ function e() {
 
     // steering ----
 
-    const convertedSteer = speed == 0 ? 0 : steerDirection * (speed / 100);
+    const convertedSteer = speed == 0 ? 0 : steerDirection * (speed / 250);
     steering += convertedSteer;
     //
     speed -= speed > 0 ? abs(convertedSteer / 500) : -abs(convertedSteer / 500);
@@ -207,13 +228,18 @@ function e() {
         ? Math.max(0, steerDirection - abs(convertedSteer))
         : Math.min(0, steerDirection + abs(convertedSteer));
 
-    if (steering > 360) steering -= 360;
-    if (steering < 0) steering += 360;
+    // steerSpeed = BASE_STEER_SPEED / (speed > 0 ? abs(speed / 2) : 1);
+    steerSpeed = Math.max(1, BASE_STEER_SPEED - abs(speed / 10));
 
-    steerSpeed = BASE_STEER_SPEED / abs(speed + 0.5 /*/ 2*/);
-    console.log("steer: ", steerSpeed);
+    console.log("steer speed: ", steerSpeed);
+    console.log("steer dir: ", steerDirection);
+    console.log("steer ang: ", steering);
+    console.log("x: ", x);
+    console.log("y: ", y);
 
     // rounding and stuff idk
+    if (steering > 360) steering -= 360;
+    if (steering < 0) steering += 360;
 
     speed = Math.round(speed * 1000) / 1000;
     steerDirection = Math.round(steerDirection * 1000) / 1000;
@@ -223,6 +249,8 @@ function e() {
     x += speed * Math.cos((steering * Math.PI) / 180);
     y += speed * Math.sin((steering * Math.PI) / 180);
     mphSpeed = Math.round(speed * 68.75);
+
+    ZOOM = BASE_ZOOM - abs(speed / 100) * BASE_ZOOM;
   }
 
   // main loop
@@ -234,7 +262,11 @@ function e() {
 
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-    console.log(speed, steering, steerDirection, x, y);
+    ctx.save();
+
+    ctx.translate(CENTER_X, CENTER_Y);
+    ctx.scale(ZOOM, ZOOM);
+    ctx.translate(-CENTER_X, -CENTER_Y);
 
     ctx.drawImage(
       backgroundImage,
@@ -244,20 +276,67 @@ function e() {
       BACKGROUND_HEIGHT
     );
 
-    rotate(
-      CANVAS_WIDTH / 2,
-      CANVAS_HEIGHT / 2,
-      (steering * Math.PI) / 180,
-      () => {
-        ctx.fillStyle = "red";
-        ctx.fillRect(
-          -PLAYER_SIZE / 2,
-          -PLAYER_SIZE / 2,
-          2 * PLAYER_SIZE,
-          PLAYER_SIZE
-        );
-      }
-    );
+    rotate(CENTER_X, CENTER_Y, (steering * Math.PI) / 180, () => {
+      ctx.fillStyle = "red";
+      ctx.fillRect(
+        -PLAYER_SIZE / 2,
+        -PLAYER_SIZE / 2,
+        2 * PLAYER_SIZE,
+        PLAYER_SIZE
+      );
+
+      // Draw wheels
+      ctx.fillStyle = "black";
+      // Front left wheel
+      ctx.save();
+      ctx.translate(PLAYER_SIZE, -PLAYER_SIZE / 2);
+      ctx.rotate((steerDirection * Math.PI) / 180);
+      ctx.fillRect(
+        -PLAYER_SIZE / 10,
+        -PLAYER_SIZE / 4,
+        PLAYER_SIZE / 2,
+        PLAYER_SIZE / 5
+      );
+      ctx.restore();
+
+      // Front right wheel
+      ctx.save();
+      ctx.translate(PLAYER_SIZE, PLAYER_SIZE / 2 - -PLAYER_SIZE / 4);
+      ctx.rotate((steerDirection * Math.PI) / 180);
+      ctx.fillRect(
+        -PLAYER_SIZE / 10,
+        -PLAYER_SIZE / 4,
+        PLAYER_SIZE / 2,
+        PLAYER_SIZE / 5
+      );
+      ctx.restore();
+      // Back left wheel
+      ctx.save();
+      ctx.translate(-PLAYER_SIZE - -PLAYER_SIZE / 1.5, -PLAYER_SIZE / 2);
+      ctx.fillRect(
+        -PLAYER_SIZE / 10,
+        -PLAYER_SIZE / 4,
+        PLAYER_SIZE / 2,
+        PLAYER_SIZE / 5
+      );
+      ctx.restore();
+
+      // Back right wheel
+      ctx.save();
+      ctx.translate(
+        -PLAYER_SIZE - -PLAYER_SIZE / 1.5,
+        PLAYER_SIZE / 2 - -PLAYER_SIZE / 4
+      );
+      ctx.fillRect(
+        -PLAYER_SIZE / 10,
+        -PLAYER_SIZE / 4,
+        PLAYER_SIZE / 2,
+        PLAYER_SIZE / 5
+      );
+      ctx.restore();
+    });
+
+    ctx.restore();
 
     drawArrow();
     drawStats();
